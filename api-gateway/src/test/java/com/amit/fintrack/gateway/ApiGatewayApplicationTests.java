bbe3cf6819fc510a5c238308d3cae1808e4a6897
Amit Gangani <amitgangani2814@gmail.com>
2026-05-01 16:21:26 +0530

    add dockerfile

---
+++ api-gateway/src/test/java/com/amit/fintrack/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.fintrack.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
